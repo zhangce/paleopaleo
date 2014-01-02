879d578d0bfb321fc9fda8a8ff1825d399406202
zhangce <czhang@cs.wisc.edu>
2014-01-02 00:12:47 -0800

    a

--- prepare_data.sh
+++ prepare_data.sh
@@ -15,13 +15,17 @@ BASE_DIR=`pwd`
 #createdb -U $DB_USER deepdive_titles
 
 #psql -U $DB_USER -c "drop schema if exists public cascade; create schema public;" $DB_NAME
-
+#
 psql -U $DB_USER -c "delete from documents;"             $DB_NAME
 psql -U $DB_USER -c "delete from entities;"             $DB_NAME
 psql -U $DB_USER -c "delete from relations_taxonomy;"             $DB_NAME
 psql -U $DB_USER -c "delete from relations_formation;"             $DB_NAME
 psql -U $DB_USER -c "delete from relations_formationtemporal;"             $DB_NAME
 psql -U $DB_USER -c "delete from relations_formationlocation;"             $DB_NAME
+psql -U $DB_USER -c "delete from relations_features_taxonomy;"             $DB_NAME
+psql -U $DB_USER -c "delete from relations_features_formation;"             $DB_NAME
+psql -U $DB_USER -c "delete from relations_features_formationtemporal;"             $DB_NAME
+psql -U $DB_USER -c "delete from relations_features_formationlocation;"             $DB_NAME
 
 psql -U $DB_USER -c "CREATE TABLE documents (id bigserial primary key, \
 											 docid  text,               \
@@ -34,7 +38,7 @@ psql -U $DB_USER -c "CREATE TABLE entities (id   bigserial primary key, \
 											 entity text,\
 											 features text);"                $DB_NAME
 
-psql -U $DB_USER -c "CREATE TABLE relations_taxonomy (id   bigserial primary key, \
+psql -U $DB_USER -c "CREATE TABLE relations_features_taxonomy (id   bigserial primary key, \
 											 docid  text,                  \
 											 type   text,                   \
 											 eid1    text,  \
@@ -42,6 +46,49 @@ psql -U $DB_USER -c "CREATE TABLE relations_taxonomy (id   bigserial primary key
 											 entity1 text,  \
 											 entity2 text,\
 											 features text,\
+											 is_correct boolean);"                $DB_NAME
+
+
+psql -U $DB_USER -c "CREATE TABLE relations_features_formation (id   bigserial primary key, \
+											 docid  text,                  \
+											 type   text,                   \
+											 eid1    text,  \
+											 eid2    text,  \
+											 entity1 text,  \
+											 entity2 text,\
+											 features text,\
+											 is_correct boolean);"                $DB_NAME
+
+
+psql -U $DB_USER -c "CREATE TABLE relations_features_formationtemporal (id   bigserial primary key, \
+											 docid  text,                  \
+											 type   text,                   \
+											 eid1    text,  \
+											 eid2    text,  \
+											 entity1 text,  \
+											 entity2 text,\
+											 features text,\
+											 is_correct boolean);"                $DB_NAME
+
+
+psql -U $DB_USER -c "CREATE TABLE relations_features_formationlocation (id   bigserial primary key, \
+											 docid  text,                  \
+											 type   text,                   \
+											 eid1    text,  \
+											 eid2    text,  \
+											 entity1 text,  \
+											 entity2 text,\
+											 features text,\
+											 is_correct boolean);"                $DB_NAME
+
+
+psql -U $DB_USER -c "CREATE TABLE relations_taxonomy (id   bigserial primary key, \
+											 docid  text,                  \
+											 type   text,                   \
+											 eid1    text,  \
+											 eid2    text,  \
+											 entity1 text,  \
+											 entity2 text,\
 											 is_correct boolean);"                $DB_NAME
 
 
@@ -52,7 +99,6 @@ psql -U $DB_USER -c "CREATE TABLE relations_formation (id   bigserial primary ke
 											 eid2    text,  \
 											 entity1 text,  \
 											 entity2 text,\
-											 features text,\
 											 is_correct boolean);"                $DB_NAME
 
 
@@ -63,7 +109,6 @@ psql -U $DB_USER -c "CREATE TABLE relations_formationtemporal (id   bigserial pr
 											 eid2    text,  \
 											 entity1 text,  \
 											 entity2 text,\
-											 features text,\
 											 is_correct boolean);"                $DB_NAME
 
 
@@ -74,8 +119,9 @@ psql -U $DB_USER -c "CREATE TABLE relations_formationlocation (id   bigserial pr
 											 eid2    text,  \
 											 entity1 text,  \
 											 entity2 text,\
-											 features text,\
 											 is_correct boolean);"                $DB_NAME
+
+
 
 
 
